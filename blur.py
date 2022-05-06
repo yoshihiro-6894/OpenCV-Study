94871d61fd727de07bf4fd5141b060c475f646e1
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-05-06 10:36:09 +0900

    Create blur.py

---
+++ blur.py
@@ -0,0 +1,30 @@
+# ブラー処理(平滑化)
+# 指定したカーネルサイズで平滑化を行う
+# サイズ領域の平均値を用いる
+
+import cv2
+
+try:
+    img = cv2.imread("./Images/Lenna.bmp")
+    #img = cv2.imread("./Images/Lenna.bmp",cv2.IMREAD_GRAYSCALE)
+    
+    if img is None:
+        print("ファイルを読み込めません")
+        import sys
+        sys.exit()
+
+    cv2.imshow("img",img)
+    
+    dst = cv2.blur(img,(15,15))
+    cv2.imwrite("blur.jpg",dst)
+    
+    cv2.imshow("dst",dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+except:
+    import sys
+    print("Error", sys.exc_info()[0])
+    print(sys.exc_info()[1])
+    import traceback
+    print(traceback.format_tb(sys.exc_info()[2]))
