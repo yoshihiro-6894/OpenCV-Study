cd74390739b40a1f4778afb63c37058575b335f7
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-05-06 13:48:07 +0900

    Create sobel.py

---
+++ sobel.py
@@ -0,0 +1,28 @@
+# Sobelフィルタを用いたエッジ検出
+
+import cv2
+
+try:
+    #img = cv2.imread("./Images/Lenna.bmp")
+    img = cv2.imread("./Images/Lenna.bmp",cv2.IMREAD_GRAYSCALE)
+    
+    if img is None:
+        print("ファイルを読み込めません")
+        import sys
+        sys.exit()
+
+    cv2.imshow("img",img)
+    
+    dst = cv2.Sobel(img, -1, 0, 1)
+    cv2.imwrite("sobel.jpg",dst)
+    cv2.imshow("dst1",dst)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+except:
+    import sys
+    print("Error", sys.exc_info()[0])
+    print(sys.exc_info()[1])
+    import traceback
+    print(traceback.format_tb(sys.exc_info()[2]))
