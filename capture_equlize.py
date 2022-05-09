4afd0b07d66cb4104cae720c5985627005000392
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-05-09 11:37:25 +0900

    Create capture_equlize.py

---
+++ capture_equlize.py
@@ -0,0 +1,28 @@
+# 接続されているカメラから映像を取り出し輝度平滑化するプログラム
+
+import cv2
+
+try:
+    capture = cv2.VideoCapture(0)
+    
+    while(True):
+        ret, frame = capture.read()
+        if ret == False:
+            print('カメラから映像を取得できませんでした。')
+            break
+       
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        dst = cv2.equalizeHist(gray)
+        cv2.imshow("f",dst)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    capture.release()
+    cv2.destroyAllWindows()
+except:
+    import sys
+    print("Error", sys.exc_info()[0])
+    print(sys.exc_info()[1])
+    import traceback
+    print(traceback.format_tb(sys.exc_info()[2]))
