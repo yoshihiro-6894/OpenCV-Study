ee3b4a920ab75ecbd7a83dc94b1f862ef3f67566
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-05-10 15:16:08 +0900

    フォルダ整理

---
+++ pythonで学ぶOpenCV4/detect_corners.py
@@ -0,0 +1,43 @@
+# 画像に含まれるコーナーを検出するプログラム
+
+import cv2
+import numpy as np
+
+try:
+    MAX_CORNERS = 50
+    BLOCK_SIZE = 3
+    QUALITY_LEVEL = 0.01
+    MIN_DISTANCE = 20.0
+    
+    img = cv2.imread("./Images/Lenna.bmp")
+    
+    if img is None:
+        print("ファイルを読み込めません")
+        import sys
+        sys.exit()
+        
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    corners = cv2.goodFeaturesToTrack(gray, MAX_CORNERS, QUALITY_LEVEL,
+                                      MIN_DISTANCE, blockSize = BLOCK_SIZE,
+                                      useHarrisDetector = False)
+    
+    print(corners)
+    
+    for i in corners:
+        x,y = i.ravel()
+        
+        x = x.astype(int)
+        y = y.astype(int)
+        cv2.circle(img, (x,y), 4, (255,255,0), 2)
+        
+    cv2.imwrite("corners.jpg",img)
+    cv2.imshow("img",img)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+except:
+    import sys
+    print("Error", sys.exc_info()[0])
+    print(sys.exc_info()[1])
+    import traceback
+    print(traceback.format_tb(sys.exc_info()[2]))
---
+++ pythonで学ぶOpenCV4/detect_objects.py
@@ -0,0 +1,36 @@
+# 画像に含まれる顔を検出するプログラム
+
+import cv2
+import numpy as np
+
+try:
+    
+    img = cv2.imread("./Images/Lenna.bmp")
+    
+    if img is None:
+        print("ファイルを読み込めません")
+        import sys
+        sys.exit()
+        
+    cascade = cv2.CascadeClassifier("./haarcascade_frontalface_alt.xml")
+    facerect = cascade.detectMultiScale(img)
+    
+    if len(facerect) > 0:
+        for rect in facerect:
+            cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]),
+                          (0,0,255), thickness=2)
+            
+    else:
+        print("no face")
+        
+    cv2.imwrite("dobj.jpg", img)
+    cv2.imshow("img",img)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+except:
+    import sys
+    print("Error", sys.exc_info()[0])
+    print(sys.exc_info()[1])
+    import traceback
+    print(traceback.format_tb(sys.exc_info()[2]))
