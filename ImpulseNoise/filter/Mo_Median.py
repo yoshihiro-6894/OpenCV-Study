325b76b34e95feae8a8026f622cea69c00771d7f
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-10-14 17:34:53 +0900

    Create saltandpepprNoise.py

--- ImpulseNoise/filter/Mo_Median.py
+++ ImpulseNoise/filter/Mo_Median.py
@@ -13,5 +13,4 @@ def median_filter(image, kernel, boundary='reflect'):
     strides = pad_image.strides * 2
     # as_strided()でpad_imageをshapeの大きさの配列をスライドしたような配列を生成する
     strided_image = np.lib.stride_tricks.as_strided(pad_image, shape, strides).reshape(shape[0], shape[1], kernel.shape[0] * kernel.shape[1])
-    #print(strided_image)
     return np.apply_along_axis(lambda a: np.median(a),2,strided_image)
---
+++ ImpulseNoise/filter/SaltAndPepperNoise.py
@@ -0,0 +1,78 @@
+'''
+https://qiita.com/bohemian916/items/9630661cd5292240f8c7
+'''
+
+import cv2
+import os
+import glob
+import numpy as np
+
+def addNoise(img, amount):
+  row,col = img.shape
+  No_amount = 0.01 * amount
+  sp_img = img.copy()
+  binary_noise = np.zeros(sp_img.shape)
+  count=0
+
+  num_salt = np.ceil(5 * No_amount * img.size)
+  print(int(num_salt))
+  mokuhyou = int(np.ceil(No_amount * img.size))
+  print("目標ノイズ個数"+str(np.ceil(No_amount * img.size)))
+  print(mokuhyou)
+  coords = [np.random.randint(0, i-1 , int(num_salt)) for i in img.shape]
+  coord= np.array(coords)
+  #print(coord.shape)
+  for i in range(coord.shape[1]):
+    if binary_noise[coord[1][i]][coord[0][i]]==0:
+      if(i%2==0):
+          sp_img[coord[1][i]][coord[0][i]]=255
+      else:
+          sp_img[coord[1][i]][coord[0][i]]=0
+      binary_noise[coord[1][i]][coord[0][i]]=255
+      count=count+1
+    
+    if count>=mokuhyou:
+      print("owari")
+      break
+      
+  count=0
+  for i in range(sp_img.shape[1]):
+    for j in range(sp_img.shape[0]):
+      if (binary_noise[j][i]==255):
+        count = count+1
+  print("付与したノイズ個数:"+str(count))
+  cv2.imwrite("noiseBinary"+str(int(amount))+"%.png",binary_noise)
+  return sp_img
+
+os.chdir("testDir")
+
+str_imgs=glob.glob('*.png')
+
+for i in range(len(str_imgs)):
+  print(str_imgs[i]+"を読み込みました")
+  input_img=cv2.imread(str_imgs[i],0)
+  dirTex="0"+str(i+1)+"n"
+  if os.path.exists(dirTex) != True:
+    os.mkdir(dirTex)
+    print("作成:"+dirTex)
+  os.chdir(dirTex)
+  cv2.imwrite("orijinal.png",input_img)
+  for j in range(9):
+    noise=addNoise(input_img,(j+1)*10)
+    cv2.imwrite("Noise"+str((j+1)*10)+"%.png",noise)
+  os.chdir("..")
+  print("\n")
+
+
+'''
+cv2.imshow("test",image)
+
+
+for i in range(9):
+  cv2.imshow("noise"+str(i),addNoise(image,(i+1)*10))
+
+print(glob.glob('*.png'))
+'''
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
